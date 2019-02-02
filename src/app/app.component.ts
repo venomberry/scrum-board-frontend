3f3d5471cd64e37251e4f3e3203d0a1cc6162bd6
venomberry <ghassenkhabacha@gmail.com>
2019-02-02 09:02:30 +0100

    state button added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { StoriesService } from './services/stories.service';
 import { Story } from './services/story';
+import { Task } from './services/task';
+import { TaskComponent } from './task/task.component';
 
 @Component({
   selector: 'app-root',
@@ -10,8 +12,19 @@ import { Story } from './services/story';
 export class AppComponent {
 
   public stories: Story[];
+  public chosenTask: Task;
 
   constructor(private storiesService: StoriesService) {
     this.stories = storiesService.stories;
+    this.chosenTask = {};
+    this.chosenTask.id = null;
+    this.chosenTask.name = '';
+    this.chosenTask.state = '';
+
+  }
+
+  log(task: Task) {
+    console.log(task);
+    this.chosenTask = task;
   }
 }
